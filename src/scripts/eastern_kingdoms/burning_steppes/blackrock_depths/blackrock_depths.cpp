3a12ab4cbd3a2241175635b571d048974120a8f7
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-10-26 10:02:10 +0200

    Fix Jubjub (#7)
    Credits VMaNGOS

--- src/scripts/eastern_kingdoms/burning_steppes/blackrock_depths/blackrock_depths.cpp
+++ src/scripts/eastern_kingdoms/burning_steppes/blackrock_depths/blackrock_depths.cpp
@@ -1597,7 +1597,7 @@ struct boss_plugger_spazzringAI : public ScriptedAI
         }
     }
 
-    void SpellHit(Unit* pCaster, SpellEntry const* pSpell) override
+    void SpellHit(SpellCaster* pCaster, SpellEntry const* pSpell) override
     {
         if (pCaster->GetTypeId() == TYPEID_PLAYER)
         {
